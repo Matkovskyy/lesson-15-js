9095ff0db92258aaaecf02e5dc3af807db0b1aa1
Matkovskyy <94220950+Matkovskyy@users.noreply.github.com>
2022-06-15 23:37:00 +0300

    Updates

---
+++ index.js
@@ -0,0 +1,17 @@
+// const Joi = require('joi');
+// const passwordSchema = Joi.string().min(3).max(10).alphanum();
+// console.log(passwordSchema.validate('qweqwe'));
+// -----VAR2 c реструктуризацией-----
+// import { generateId, sum } from "./mylibrary.js";
+console.log(`Module 8.1`);
+// console.log(generateId());
+// console.log(sum(1, 1));
+
+// ------VAR 1------
+// import * as obj from "./mylibrary.js";
+// console.log(obj);
+// console.log(obj.generateId());
+// console.log(obj.sum(1, 1));
+
+
+
